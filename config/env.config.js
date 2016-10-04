3ff1950ea4212dd1953c1133be04433336dfe5fa
murilo <macquadros@gmail.com>
2016-10-04 10:31:27 -0300

    feat(env.config) creating config object

--- config/env.config.js
+++ config/env.config.js
@@ -1,1 +1,12 @@
 'use strict';
+module.exports = {
+    port: 6000,
+    databases: {
+        devdb: {
+            host: '192.168.100.121',
+            user: 'pdg',
+            password: 'Senha!235',
+            database: 'jqueue'
+        }
+    }
+};
