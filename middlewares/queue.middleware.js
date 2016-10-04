e48d6419358658e734c14aee7c6b8ec7d511cc30
murilo <macquadros@gmail.com>
2016-10-04 12:02:45 -0300

    feat(queue.middleware) getting queue

---
+++ middlewares/queue.middleware.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function (req, res, next) {
+
+    if (req.params && req.params.queue) {
+        var queueName = req.params.queue;
+        req.jqueue.use(queueName, true, function (error, queue) {
+            if (!error) {
+                console.log(queue);
+                req.queue = queue;
+                next();
+            } else {
+                if (error.message.match(/ER_NO_SUCH_TABLE/)) {
+                    res.send(404, {message: 'queue ' + queueName + ' not found'});
+                } else {
+                    res.send(500, error);
+                }
+            }
+        });
+    } else {
+        res.send(400, {message: 'missing queue'});
+    }
+
+};
